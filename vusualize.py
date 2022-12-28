8f30047aa62723ffa0166f574c1235042286671a
frikky <fredrik_9490@hotmail.com>
2022-12-28 23:29:44 +0100

    Added useless files

--- vusualize.py
+++ vusualize.py
@@ -37,6 +37,7 @@ boxsize = 50
  
 displaysurface = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Halvz")
+basepos = WIDTH-INNRWIDTH/2 
 
 iterations = 0
 start.init()
@@ -64,15 +65,13 @@ class Block(pygame.sprite.Sprite):
        # Fetch the rectangle object that has the dimensions of the surf
        # Update the position of this object by setting the values of rect.x and rect.y
         self.rect = self.surf.get_rect()
-        basepos = WIDTH-INNRWIDTH/2 
-        #self.pos = vec((xpos, 150))
 
         self.rect.x = WIDTH-int(basepos)+(INNRWIDTH/maxlen*input_object["x"]-60)
         self.rect.y = WIDTH-int(basepos)+(INNRWIDTH/maxlen*input_object["y"]-60)
 
         self.object = input_object.copy()
 
-    def explode(self):
+    def explode(self, y, x):
         exp = images = [
             pygame.image.load("imgs/xp1.png"),
             pygame.image.load("imgs/xp2.png"),
@@ -83,11 +82,17 @@ class Block(pygame.sprite.Sprite):
         ]
 
         mixer.Sound.play(crash_sound)
+        rect = self.rect
+        
+        xstart = WIDTH-int(basepos)+(INNRWIDTH/maxlen*x-60)
+        ystart = WIDTH-int(basepos)+(INNRWIDTH/maxlen*y-60)
+        print("Rect: x: %s, y: %s" % (xstart, ystart))
         for item in exp:
-            displaysurface.blit(item, (self.rect.x, self.rect.y))
+            displaysurface.blit(item, (xstart, ystart))
             pygame.display.update()
             time.sleep(0.1)
 
+        print()
         #soundObj = mixer.Sound('explode.mp3')
         #soundObj = mixer.music.load(r'explode.mp3')
         #soundObj.play()
@@ -101,122 +106,6 @@ class Block(pygame.sprite.Sprite):
 
 
         #self.rect.midbottom = self.pos
-
-class Box(pygame.sprite.Sprite):
-    def __init__(self, input_object):
-        super().__init__()
-        #self.surf = pygame.Surface((INNRWIDTH/maxlen, INNRWIDTH/maxlen))
-        #self.surf.fill((128,255,40))
-        #self.rect = self.surf.get_rect()
-
-        #self.rect = self.surf.get_rect(center = (WIDTH/2, INNRWIDTH+INNRWIDTH/2))
-        #print(input_object, xpos)
-
-        #self.surf = pygame.Surface((30, 30))
-        self.surf = pygame.Surface((INNRWIDTH/maxlen-1, INNRWIDTH/maxlen-1))
-
-        if input_object["flag"] == 1:
-            self.surf.fill((187,37,40))
-        elif input_object["flag"] == 2:
-            self.surf.fill((20,107,58))
-        else: 
-            self.surf.fill((0,255,0))
-
-        self.rect = self.surf.get_rect()
-
-        basepos = WIDTH-INNRWIDTH/2 
-        xpos = WIDTH-int(basepos)+(INNRWIDTH/maxlen*input_object["x"]-60)
-        self.pos = vec((xpos, 150))
-        self.vel = vec(0,0)
-        self.acc = vec(0,0)
-        self.object = input_object.copy()
-
-        #self.rect.midbottom = self.pos
-
-    def explode(self):
-        exp = images = [
-            pygame.image.load("imgs/xp1.png"),
-            pygame.image.load("imgs/xp2.png"),
-            pygame.image.load("imgs/xp3.png"),
-            pygame.image.load("imgs/xp4.png"),
-            pygame.image.load("imgs/xp5.png"),
-            pygame.image.load("imgs/xp6.png"),
-        ]
-
-        mixer.Sound.play(crash_sound)
-        for item in exp:
-            displaysurface.blit(item, (self.pos.x-37, self.pos.y-72))
-            pygame.display.update()
-            time.sleep(0.1)
-
-        #soundObj = mixer.Sound('explode.mp3')
-        #soundObj = mixer.music.load(r'explode.mp3')
-        #soundObj.play()
-        #soundObj.play()
-
-        #pygame.mixer.Sound.play(soundObj)
-
-        #self.pos.y = 0 
-        #self.pos.x = 0 
-        #self.surf.fill((0,0,0))
-
-
-        self.rect.midbottom = self.pos
-
-    def move_down(self, pixel):
-        if pixel["x"] != self.object["x"]:
-            print("change: %d vs %d" % (pixel["x"], self.object["x"]))
-
-        if pixel["y"] >= 8 or self.object["y"] >= 8:
-            self.surf.fill([0,0,0])
-            return
-
-        if pixel["y"] == self.object["y"]:
-            return
-
-        self.acc.y = ACC
-
-        self.pos.y = self.pos.y + INNRWIDTH/maxlen
-        self.acc.y += self.vel.y * FRIC
-
-        self.vel += self.acc
-        self.pos += self.vel + 0.5 * self.acc
-
-        self.rect.midbottom = self.pos
-        self.object = pixel.copy()
-        #pygame.display.update()
-
-    def manual_move(self):
-        self.acc = vec(0,0)
-
-        pressed_keys = pygame.key.get_pressed()
-
-        if pressed_keys[K_LEFT]:
-            self.acc.x = -ACC
-        if pressed_keys[K_RIGHT]:
-            self.acc.x = ACC
-
-        if pressed_keys[K_UP]:
-            self.acc.y = -ACC
-        if pressed_keys[K_DOWN]:
-            self.acc.y = ACC
-
-        self.acc.x += self.vel.x * FRIC
-        self.acc.y += self.vel.y * FRIC
-        self.vel += self.acc
-        self.pos += self.vel + 0.5 * self.acc
-
-        if self.pos.x > WIDTH:
-            self.pos.x = 0
-        if self.pos.x < 0:
-            self.pos.x = WIDTH
-
-        if self.pos.y > HEIGHT:
-            self.pos.y = 0
-        if self.pos.y < 0:
-            self.pos.y = HEIGHT
-
-        self.rect.midbottom = self.pos
 
 class platform(pygame.sprite.Sprite):
     def __init__(self):
@@ -262,8 +151,8 @@ while True:
     for dels in deletes:
         for item in objects:
             if item.object["uuid"] == dels[2]:
-                #print("Del y: %d, x:%d, uuid: %s" % (dels[0], dels[1], dels[2]))
-                item.explode()
+                print("Del y: %d, x:%d, uuid: %s" % (dels[0], dels[1], dels[2]))
+                item.explode(dels[0], dels[1])
                 break
 
     start.deletes = []
@@ -275,6 +164,7 @@ while True:
     if new_world == None:
         print("NONE")
         continue
+
     for line in new_world:
         # print("line", line)
         for pixel in line:
